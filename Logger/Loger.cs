76c38b16a1ab3e23f750a9d8d2a3e1ea4f957f10
sdvbjdkjv <riuihuirgfsdeuf.com>
2021-08-28 17:11:58 +0300

    Loger mb done

---
+++ Logger/Loger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace Loger
+{
+    class Loger
+    {
+        private string log;
+
+        private string config;
+
+        private string mode;
+        public Loger() { }
+        public Loger(string log, string config)
+        {
+            this.log = log;
+            this.config = config;
+            FileInfo finfo = new FileInfo(log);
+            using (FileStream fs = finfo.Open(FileMode.OpenOrCreate))
+            {
+
+            };
+
+            using (StreamWriter writer = File.AppendText(log))
+            {
+                writer.WriteLine($"Log file create at {DateTime.Now}");
+            }
+
+
+            using (StreamReader reader = File.OpenText(this.config))
+            {
+                mode = reader.ReadLine();
+            }
+        }
+
+        public void CreateLog(string mesType, string uName, string message) //write method
+        {
+            if (mode == "Base")
+            {
+
+                using (StreamWriter writer = File.AppendText(log))
+                {
+                    writer.WriteLine($"|Время записи: {DateTime.Now}||Тип: {mesType}|");
+                }
+
+            }
+            else if (mode == "Mid")
+            {
+                using (StreamWriter writer = File.AppendText(log))
+                {
+                    writer.WriteLine($"|Время записи: {DateTime.Now}||Тип: {mesType}||Пользователь: {uName}|");
+                }
+            }
+            else if (mode == "Full")
+            {
+                using (StreamWriter writer = File.AppendText(log))
+                {
+                    writer.WriteLine($"|Время записи: {DateTime.Now}||Тип: {mesType}||Пользователь: {uName}|| Текст: {message}|");
+                }
+            }
+            else
+            {
+                WriteLine("Ошибка конфигурационного файла");
+            }
+        }
+        public void GetLogs() //read method
+        {
+
+            foreach (string i in File.ReadAllLines(log))
+            {
+                WriteLine(i);
+            }
+        }
+
+    }
+}
--- Logger/Program.cs
+++ Logger/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
-namespace Logger
+namespace Loger
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Loger loger = new Loger("Loger.txt", "Config.ini");
+            loger.CreateLog("Test message", "User_1", "123");
+            loger.GetLogs();
         }
     }
 }
